0ddad6405f8bfd8204a7fbe3893146d242019eac
IvanMerejko <32937297+IvanMerejko@users.noreply.github.com>
2019-09-11 09:10:55 +0300

    fixed problem with tables

--- АМО/lab1.cpp
+++ АМО/lab1.cpp
@@ -1,183 +1,121 @@
 #include <iostream>
-#include <stdio.h>
-#include <math.h>
+#include <cmath>
 #include <iomanip>
 #include <tuple>
-#include <functional>
 #include <thread>
 
-
-/*
-   TODO
-   write function to count value for sin, sh, exp, ln
-
-*/
-
 using returnableTuple = std::tuple<double, double, int>;
+
+
 class Task
 {
 private:
    enum class NumberOfTask
    {
-      First , 
+      First,
       Second
    };
 private:
    double m_a;
    double m_b;
+   const double m_PI;
    double m_x;
-   double m_PI;
-   double m_nForSecondTask;
+   mutable double m_nForSecondTask;
+
 public:
-  
+
    Task();
    Task(const double a, const double b);
-   void createFirstTable();
+   void createFirstTable() const;
    void createSecondTable() const;
 private:
-   returnableTuple getCosFunctionValue(double x, NumberOfTask task, double eps = 0.0) const;
-   returnableTuple getSinFunctionValue(double x, NumberOfTask task, double eps = 0.0) const;
-   returnableTuple getShFunctionValue(double x, NumberOfTask task, double eps = 0.0) const;
-   returnableTuple getChFunctionValue(double x, NumberOfTask task, double eps = 0.0) const;
-   returnableTuple getExpFunctionValue(double x, NumberOfTask task, double eps = 0.0) const;
-   returnableTuple getLnFunctionValue(double x, NumberOfTask task, double eps = 0.0) const;
+   returnableTuple getSinFunctionValue(NumberOfTask task, double x = 0.0, double eps = 0.0 ) const;
 };
 
 Task::Task()
-   : Task(0 , 0)
+   : Task(0, 0)
 {}
 Task::Task(const double a, const double b)
    : m_a(a)
    , m_b(b)
-   , m_x((m_a + m_b) / 2)
    , m_PI(3.14159265)
+   , m_x( ((m_a + m_b) /2) * m_PI / 180)
+   , m_nForSecondTask(0)
 {}
-void Task::createFirstTable()
+void Task::createFirstTable() const
 {
-   std::cout <<                  "eps" 
-             << std::setw(17) << "n"   
-             << std::setw(8)  << "fluff" 
-             << std::setw(21) << "last (R)" 
-             << std::endl << std::endl;
-   auto step = std::pow(10, -3);
-   auto limit = std::pow(10, -14);
-   for (auto eps = std::pow(10, -2); eps > limit; eps *= step)
+   std::cout << "First Table\n\n";
+   std::cout << "eps"
+      << std::setw(10) << "n"
+      << std::setw(12) << "abs_delta"
+      << std::setw(21) << "last (R)"
+      << std::endl << std::endl;
+   const auto step = 1e-3;
+   const auto limit = 1e-14;
+   for (auto eps = 1e-2; eps > limit; eps *= step)
    {
       double R;
       int n;
-      double fluff;
-      std::tie(fluff, R, n) = getSinFunctionValue(m_PI / 2 - m_x , NumberOfTask::First, eps);
-      fluff = fabs(cos(m_PI / 2 - m_x) - fluff);
-      std::cout << std::setw(6)  << eps 
-                << std::setw(4)  << n 
-                << std::setw(18) << fluff 
-                << std::setw(18) << R 
-                << std::endl;
+      double result;
+      std::tie(result, R, n) = getSinFunctionValue(NumberOfTask::First,m_x, eps);
+      std::cout << std::setprecision(2) << std::scientific
+         << std::setw(6)  << eps
+         << std::setw(6)  << n
+         << std::setw(12) << abs(abs(sin(m_x)) - abs(result))
+         << std::setw(18) << R
+         << std::endl;
 
-      if (eps == pow(10, -8))
+      if (eps == 1e-8)
          m_nForSecondTask = n;
    }
 
 }
-returnableTuple Task::getCosFunctionValue(double x, NumberOfTask task, double eps) const
+returnableTuple Task::getSinFunctionValue(NumberOfTask task, double x,  double eps) const
 {
-   double U = 1.0, result = 1.0;
-   int k = 1;
-   auto lambda = [](double x , int k , double U)
+   auto U = x, result = x;
+   auto k = 1;
+   auto lambda = [=](const double x, const int k,const double U)
    {
-      return -(std::pow(x, 2) / (2 * k * (2 * k - 1))) * U;
+      return -x * x / ( (2*k + 1)* 2*k)  * U;
    };
    if (task == NumberOfTask::First)
    {
-      while (std::fabs(U) > eps)
+      do
       {
-         U = lambda(x, k, U);
+         U = lambda(m_x, k, U);
          result += U;
-         k++;
-      }
-   } 
-   else
-   {
-      for (; k < m_nForSecondTask; ++k)
-      {
-         U = lambda(x, k, U);
-         result += U;
-      }
-   }
-   return std::make_tuple(result , U , k);
-}
-returnableTuple Task::getSinFunctionValue(double x, NumberOfTask task, double eps) const
-{
-   double U = x, result = 1.0;
-   int k = 1;
-   auto lambda = [](double x, int k, double U)
-   {
-      return -(std::pow(x, 2) / (2 * k * (2 * k + 1))) * U;
-   };
-   if (task == NumberOfTask::First)
-   {
-      while (std::fabs(lambda(x, k, U)) > eps)
-      {
-         U = lambda(x, k, U);
-         result += U;
-         k++;
-      }
+         k += 1;
+      } while (abs(lambda(m_x, k, U)) > eps);
+
    }
    else
    {
-      for (; k < m_nForSecondTask; ++k)
+      for(; k < m_nForSecondTask ; ++k)
       {
          U = lambda(x, k, U);
          result += U;
       }
    }
-   return std::make_tuple(result, U, k);
-}
-returnableTuple Task::getChFunctionValue(double x, NumberOfTask task, double eps) const
-{
-   double U = x, result = 1.0;
-   int k = 1;
-   auto lambda = [](double x, int k, double U)
-   {
-      return -(std::pow(x, 2) / (2 * k * (2 * k - 1))) * U;
-   };
-   if (task == NumberOfTask::First)
-   {
-      while (std::fabs(U) > eps)
-      {
-         U = lambda(x, k, U);
-         result += U;
-         k++;
-      }
-   }
-   else
-   {
-      for (; k < m_nForSecondTask; ++k)
-      {
-         U = lambda(x, k, U);
-         result += U;
-      }
-   }
-   return std::make_tuple(result, U / 3, k);
+   return std::make_tuple(result, lambda(x, k, U), k);
 }
 void Task::createSecondTable() const
 {
-   double h = (m_b - m_a) / 10;
-   std::cout <<                  "x_i" 
-             << std::setw(21) << "fluff" 
-             << std::setw(21) << "last (R)" 
-             << std::endl << std::endl;
-   for (int i = 0; i < 10; i++) 
+   std::cout << "\n\nSecond Table\n" << std::endl;
+   const auto h = (m_b - m_a) / 10;
+   std::cout << "x_i"
+      << std::setw(21) << "abs_delta"
+      << std::setw(21) << "last (R)"
+      << std::endl << std::endl;
+   for (int i = 0; i < 10; i++)
    {
-      double x = m_a + h * i;
+      const auto x = m_a + h * i;
       double R;
-      double fluff;
+      double result;
       int unused;
-      std::tie(fluff, R, unused) = getSinFunctionValue(m_PI / 2 - x, NumberOfTask::Second);
-      fluff = fabs(cos(m_PI / 2 - x) - fluff);
-      std::cout << std::setw(7)  << std::setprecision(4) << x
-                << std::setw(22) << std::setprecision(14) << fluff
-                << std::setw(22)  << fabs(R) << std::endl;
+      std::tie(result, R, unused) = getSinFunctionValue(NumberOfTask::Second, x*m_PI / 180);
+      std::cout << std::defaultfloat << std::setw(7) << std::setprecision(4) << x
+         << std::setw(17) << abs(abs(sin(x*m_PI / 180)) - abs(result))
+         << std::setw(22) << abs(R) << std::endl;
    }
    std::cout << std::endl;
 }
