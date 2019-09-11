c39fbc51c77e73a440b13b0bbd974a8323706415
IvanMerejko <32937297+IvanMerejko@users.noreply.github.com>
2019-09-11 13:33:01 +0300

    some change in drawing

--- КГ/lab2.py
+++ КГ/lab2.py
@@ -1,17 +1,21 @@
-
-
-# Import all definitions from tkinter
-
 from tkinter import *
-import datetime
-
 PIXEL_SIZE = 2
 HEIGHT = 500
 WIDTH = 800
 
 
+def create_button(frame, name, callback, column_number):
+    button = Button(frame, text=name, command=callback)
+    button.grid(row=1, column=column_number)
+
+
 class RasterizationAlgorithms:
+    start_points = [[30, 70], [300, -70], [270, 180], [200, 170], [250, 100], [300, 80], [380, 80]]
     points = []
+    scale = 1
+    shiftY = 0
+    shiftX = 0
+
 
     def factorial(self, n):
         return 1 if n <= 1 else n * self.factorial(n-1)
@@ -31,11 +35,17 @@ class RasterizationAlgorithms:
                 self.points[ind][1] += arr[i][1] * b
             t += step
 
-    def draw_bazier_line(self, arr):
-        self.set_bezier_points(arr)
+    def clean(self):
+        self.canvas.delete("bazier_line")
+
+    def draw_bazier_line(self):
+        self.set_bezier_points(self.start_points)
         for point in self.points:
-            self.canvas.create_rectangle(point[0], point[1],
-                                         point[0], point[1])
+            self.canvas.create_rectangle( (point[0] + self.shiftX) * self.scale,
+                                          (point[1] + self.shiftY) * self.scale,
+                                          (point[0] + self.shiftY) * self.scale,
+                                          (point[1] + self.shiftY) * self.scale,
+                                          tag="bazier_line")
 
     def __init__(self):
         window = Tk()
@@ -46,9 +56,11 @@ class RasterizationAlgorithms:
         # Draw Frame in the window
         frame = Frame(window, bg="light blue")
         frame.pack()
-        self.draw_bazier_line([[30, 70], [300, -70], [270, 180], [200, 170], [250, 100], [300, 80], [380, 80]])
+
+        self.draw_bazier_line()
 
         window.mainloop()
+
 
 
 if __name__ == "__main__":
