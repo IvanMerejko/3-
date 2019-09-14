f2023a694cd3376693ebae462bfca4f8550d9143
IvanMerejko <32937297+IvanMerejko@users.noreply.github.com>
2019-09-14 15:16:07 +0300

    changed Zedel iteration method to simple iteration method

--- АМО/lab3.cpp
+++ АМО/lab3.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <array>
 #include <algorithm>
+
 constexpr int LineNumber = 4;
 constexpr int ColumnNumber = 5;
 using Line = std::array<double, ColumnNumber>;
@@ -15,29 +16,29 @@ public:
    explicit Task(const SLAR& system);
    void solveBySingleDivisionScheme() const;
    void solveByGaussJordanExclusion() const;
-   void solveByZedelItaration() const;
+   void solveBySimpleIteration() const;
    void printSlar() const;
 private:
    //for SingleDivisionScheme
    void simplifyMatrixByLine(SLAR& system, int line) const;
    //for GaussJordanExclusion
    void subtractionTwoLinesToReduceVariable(SLAR& system, int from, int which) const;
-   bool converge(const solutionType& current, const solutionType& previouse) const;
 };
 
 int main()
 {
-   SLAR slar = 
-   {  
-                  43, -5, 32, 0, -15 ,
-                  12, 32, 18, 3, 114 ,
-                  20, 1, 27, 5, 3 ,
-                  -10, -27, -5, 50, -81  
+   SLAR slar =
+   {
+               43, -5, 32, 0, -15 ,
+               12, 32, 18, 3, 114 ,
+               20, 1, 27, 5, 3 ,
+               -10, -27, -5, 50, -81
    };
    Task task(slar);;
    task.solveBySingleDivisionScheme();
    task.solveByGaussJordanExclusion();
-   
+   task.solveBySimpleIteration();
+
    std::cin.get();
 }
 
@@ -45,36 +46,47 @@ Task::Task(const SLAR & system)
    : m_system(system)
 {
 }
-void Task::solveByZedelItaration() const
+void Task::solveBySimpleIteration() const
 {
+   std::cout << "\nSolveByZedelIteration\n";
    auto matrix = m_system;
-   solutionType curretnSolution, previousSolution;
+   solutionType  previousSolution;
 
-   do
+   while(true)
    {
-      for (int i = 0; i < LineNumber; i++)
-         previousSolution[i] = curretnSolution[i];
-      for (int i = 0; i < LineNumber; i++)
+      solutionType currentSolution;
+      for(int i = 0; i < LineNumber; ++i)
       {
-         double var = 0;
-         for (int j = 0; j < i; j++)
-            var += (matrix[i][j] * curretnSolution[j]);
-         for (int j = i + 1; j < LineNumber; j++)
-            var += (matrix[i][j] * previousSolution[j]);
-         curretnSolution[i] = (matrix[ColumnNumber - 1][i] - var) / matrix[i][i];
+         currentSolution[i] = matrix[i][ColumnNumber - 1];
+
+         for(int j = 0 ; j < LineNumber ; ++j)
+         {
+            if(i != j)
+            {
+               currentSolution[i] -= matrix[i][j] * previousSolution[j];
+            }
+         }
+         currentSolution[i] /= matrix[i][i];
       }
-   } while (!converge(x, p));
+      long double error = 0;
+      for (int i = 0; i < LineNumber; ++i)
+      {
+         error += abs(currentSolution[i] - previousSolution[i]);
+      }
+      if (error < 1e-8)
+      {
+         break;
+      }
+      previousSolution = currentSolution;
+   }
+
+   std::copy(previousSolution.begin(), previousSolution.end(), std::ostream_iterator<double>(std::cout, " "));
 
 }
-bool Task::converge(const solutionType& current, const solutionType& previouse) const
-{
-   double norm = 0;
-   for (int i = 0; i < LineNumber; i++)
-      norm += (current[i] - previouse[i])*(current[i] - previouse[i]);
-   return (sqrt(norm)<eps);
-}
+
 void Task::solveBySingleDivisionScheme() const
 {
+   std::cout << "\nSolveBySingleDivisionScheme\n";
    auto matrix = m_system;
    simplifyMatrixByLine(matrix, 0);
    simplifyMatrixByLine(matrix, 1);
@@ -83,18 +95,19 @@ void Task::solveBySingleDivisionScheme() const
    solution[0] = matrix[LineNumber - 1][ColumnNumber - 1] / matrix[LineNumber - 1][ColumnNumber - 2]; //< x4
    solution[1] = matrix[LineNumber - 2][ColumnNumber - 1] - matrix[LineNumber - 2][ColumnNumber - 2] * solution[0];//< x3
    solution[2] = matrix[LineNumber - 3][ColumnNumber - 1] - matrix[LineNumber - 3][ColumnNumber - 2] * solution[0]//< x2
-                                                          - matrix[LineNumber - 3][ColumnNumber - 3] * solution[1];
+                                             - matrix[LineNumber - 3][ColumnNumber - 3] * solution[1];
    solution[3] = matrix[LineNumber - 4][ColumnNumber - 1] - matrix[LineNumber - 4][ColumnNumber - 2] * solution[0] //< x1
-                                                          - matrix[LineNumber - 4][ColumnNumber - 3] * solution[1]
-                                                          - matrix[LineNumber - 4][ColumnNumber - 4] * solution[2];
+                                             - matrix[LineNumber - 4][ColumnNumber - 3] * solution[1]
+                                             - matrix[LineNumber - 4][ColumnNumber - 4] * solution[2];
    std::reverse(solution.begin(), solution.end());
 
-   std::cout << "\n\nResult: \n";
+   std::cout << "Result: \n";
    std::copy(solution.begin(), solution.end(), std::ostream_iterator<double>(std::cout, " "));
 }
 
 void Task::solveByGaussJordanExclusion() const
 {
+   std::cout << "\nSolveByGaussJordanExclusion\n";
    auto matrix = m_system;
    simplifyMatrixByLine(matrix, 0);
    simplifyMatrixByLine(matrix, 1);
@@ -115,9 +128,9 @@ void Task::solveByGaussJordanExclusion() const
    solution[1] = matrix[1][ColumnNumber - 1];//< x2
    solution[2] = matrix[2][ColumnNumber - 1];//< x3
    solution[3] = matrix[3][ColumnNumber - 1];//< x4
-   std::cout << "\n\nResult: \n";
+   std::cout << "Result: \n";
    std::copy(solution.begin(), solution.end(), std::ostream_iterator<double>(std::cout, " "));
-} 
+}
 
 void Task::simplifyMatrixByLine(SLAR& system, int line) const
 {
@@ -131,7 +144,7 @@ void Task::simplifyMatrixByLine(SLAR& system, int line) const
       auto firstElement = system[row][line];
       for (int column = 0; column < m_system[0].size(); ++column)
       {
-         system[row][column] -= firstElement*system[line][column];
+         system[row][column] -= firstElement * system[line][column];
       }
    }
 }
@@ -153,3 +166,4 @@ void Task::printSlar() const
       std::cout << std::endl;
    }
 }
+
