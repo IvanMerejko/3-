937924c17a8de966f14e72e9a4290d5fc764adac
IvanMerejko <32937297+IvanMerejko@users.noreply.github.com>
2019-09-19 08:24:12 +0300

    added rotation

--- КГ/lab2.py
+++ КГ/lab2.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import math
 PIXEL_SIZE = 2
 HEIGHT = 500
 WIDTH = 800
@@ -15,6 +16,8 @@ class RasterizationAlgorithms:
     scale = 1
     shiftY = 0
     shiftX = 0
+    angle = 0
+    d_angle = 0.1
 
 
     def factorial(self, n):
@@ -52,31 +55,50 @@ class RasterizationAlgorithms:
         self.scale = 1
         self.shiftY = 0
         self.shiftX = 0
-
+        self.angle = 0
         self.set_bezier_points(self.start_points)
         self.draw_bazier_line()
 
     def alienate(self):
         if self.scale > 0.1:
             self.scale -= 0.1
+        self.angle = 0
         self.draw_bazier_line()
 
     def bring_closer(self):
         if self.scale < 2:
             self.scale += 0.1
         self.set_bezier_points(self.start_points)
+        self.angle = 0
         self.draw_bazier_line()
 
     def mirror_by_x(self):
         self.set_bezier_points(self.start_points)
+        self.angle = 0
         for point in self.points:
-            point[0] = HEIGHT - point[0]
+            point[1] = HEIGHT - point[1]
         self.draw_bazier_line()
 
     def mirror_by_y(self):
         self.set_bezier_points(self.start_points)
+        self.angle = 0
         for point in self.points:
-            point[1] = WIDTH - point[1]
+            point[0] = WIDTH - point[0]
+        self.draw_bazier_line()
+
+    def rotate_up(self):
+        self.angle += 0.01
+        self.rotate()
+
+    def rotate_down(self):
+        self.angle -= 0.01
+        self.rotate()
+
+    def rotate(self):
+        self.set_bezier_points(self.start_points)
+        for point in self.points:
+            point[0] = point[0] * math.cos(self.angle) + point[1] * math.sin(self.angle)
+            point[1] = point[1] * math.cos(self.angle) - point[0] * math.sin(self.angle)
         self.draw_bazier_line()
 
     def __init__(self):
@@ -94,6 +116,8 @@ class RasterizationAlgorithms:
         create_button(frame, "bring_closer", self.bring_closer, 3)
         create_button(frame, "mirror_by_x", self.mirror_by_x, 4)
         create_button(frame, "mirror_by_y", self.mirror_by_y, 5)
+        create_button(frame, "rotate_up", self.rotate_up, 6)
+        create_button(frame, "rotate_down", self.rotate_down, 7)
 
         window.mainloop()
 
