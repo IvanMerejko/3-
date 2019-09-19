66e234a331a940e4d5b516dbe7a08da182fe10a4
IvanMerejko <32937297+IvanMerejko@users.noreply.github.com>
2019-09-19 06:59:52 +0300

    fixed issues

--- АМО/lab2.cpp
+++ АМО/lab2.cpp
@@ -1,226 +1,199 @@
 #include <iostream>
-#include <stdio.h>
-#include <stdlib.h>
 #include <cmath>
 #include <iomanip>
-#include <chrono>
-
-/*
-   TODO
-   How calculate a,b,x,m,M ?
-   For another two methods need solve the equation f(x)*f(x)'' > 0 
-*/
+#include <vector>
 
 class Task
 {
-   double m_x;
    double m_a;
    double m_b;
    double m_m;
    double m_M;
-   double m_c;
-   int m_nForIterationMethod;
-   int m_nForBisectionMethod;
-   int m_nForChordMethod;
-   int m_nForTangentMethod;
+   double m_step;
+   mutable std::vector<int> m_nForIterationMethod;
+   mutable std::vector<int>  m_nForBisectionMethod;
+   mutable std::vector<double> m_mas;
+
 public:
-   Task(double x , double a, double b, double m, double M);
-   Task(double x, double a, double b, double m, double M , double c);
+   Task(double a, double b);
    void createTableForIterationMethod();
    void createTableForBisectionMethod();
-   void createTableForChordMethod();
-   void createTableForTangentMethod();
-   void createThirdTable();
+   void createThirdTable() const;
 private:
    double getFunctionValue(double x) const;
-   
+   double getFirstDerivativeValue(double x) const;
+   void createMass() const;
+   void createConstants(double a, double b);
 
-   double getByIterationMethod(double eps);
-   double getByBisectionMethod(double a, double b, double eps);
-   double getByChordMethod(double eps);
-   double getByTangentMethod(double eps);
+
+   std::tuple<double, double> getByIterationMethod(double a, double b, double eps) const;
+   std::tuple<double, double> getByBisectionMethod(double a, double b, double eps) const;
 
 };
 
-Task::Task(double x, double a, double b, double m, double M , double c)
-   : m_x(x) 
-   , m_a(a)
+Task::Task(double a, double b)
+   : m_a(a)
    , m_b(b)
-   , m_m(m)
-   , m_M(M)
-   , m_c(c)
-   , m_nForIterationMethod(0)
-   , m_nForBisectionMethod(0)
-   , m_nForChordMethod(0)
-   , m_nForTangentMethod(0)
-{}
-Task::Task(double x, double a, double b, double m, double M)
-   : Task(x, a, b, m, M, 0)
-{}
+   , m_m(0)
+   , m_M(0)
+   , m_step(0.2)
+{
+
+   createMass();
+}
+void Task::createConstants(double a, double b)
+{
+   if (fabs(getFirstDerivativeValue(a)) > fabs(getFirstDerivativeValue(b)))
+   {
+      m_M = getFirstDerivativeValue(a);
+      m_m = getFirstDerivativeValue(b);
+   }
+   else
+   {
+      m_M = getFirstDerivativeValue(b);
+      m_m = getFirstDerivativeValue(a);
+   }
+}
+
+void Task::createMass() const
+{
+   for(double a = m_a; a <= m_b; a += m_step)
+   {
+      if(getFunctionValue(a) * getFunctionValue(a+m_step) < 0)
+      {
+         m_mas.push_back(a);
+      }
+   }
+}
 
 void Task::createTableForIterationMethod()
 {
-   auto step = std::pow(10, -3);
-   auto limit = std::pow(10, -14);
-   std::cout << "IterationMethod\n";
-   std::cout << "\tEps" << "\t\tRoot's value\t" << "\tEstimation accuracy" << std::endl;
-   for (auto eps = std::pow(10, -2); eps > limit; eps *= step)
+   const auto step = 1e-3;
+   const auto limit = 1e-14;
+   std::cout << "-----------------------------------\n" <<"IterationMethod\n";
+   double a{};
+   double b{};
+      
+   for(size_t i = 0 ; i < m_mas.size() ; ++i)
    {
-      std::cout << std::fixed << std::setprecision(15) << eps << '\t' << m_x << '\t' << std::abs(getByIterationMethod(eps) - m_x) << std::endl;
+      std::cout << "\tEps" << "\t\tRoot_" << i + 1  << "\t\t\taccurately" << std::endl;
+      a = m_mas[i];
+      b = a + m_step;
+      createConstants(a, b);
+      for (auto eps = 1e-2; eps > limit; eps *= step)
+      {
+         double my_x{};
+         double accurately{};
+         std::tie(my_x, accurately) = getByIterationMethod(a, b, eps);
+         std::cout << std::fixed << std::setprecision(15) << eps  << '\t' << my_x << '\t' << accurately << std::endl;
+      }
    }
+   std::cout << "\n-----------------------------------\n";
 }
 
 void Task::createTableForBisectionMethod()
 {
-   auto step = std::pow(10, -3);
-   auto limit = std::pow(10, -14);
-   std::cout << "BisectionMethod\n";
-   std::cout << "\tEps" << "\t\tRoot's value\t" << "\tEstimation accuracy" << std::endl;
-   for (auto eps = std::pow(10, -2); eps > limit; eps *= step)
+   const auto step = 1e-3;
+   const auto limit = 1e-14;
+   std::cout << "----------------------------------- \n" <<"BisectionMethod\n";
+   double a{};
+   double b{};
+
+   for (size_t i = 0; i < m_mas.size(); ++i)
    {
-      std::cout << std::fixed << std::setprecision(15) << eps << '\t' << m_x << '\t' << std::abs(getByBisectionMethod(m_a, m_b, eps) - m_x) << std::endl;
+      std::cout << "\tEps" << "\t\tRoot_" << i + 1 << "\t\t\taccurately" << std::endl;
+      a = m_mas[i];
+      b = a + m_step;
+      for (auto eps = 1e-2; eps > limit; eps *= step)
+      {
+         double my_x{};
+         double accurately{};
+         std::tie(my_x, accurately) = getByBisectionMethod(a, b, eps);
+         std::cout << std::fixed << std::setprecision(15) << eps << '\t' << my_x << '\t' << accurately << std::endl;
+      }
    }
+   std::cout << "\n-----------------------------------\n";
 }
 
-void Task::createTableForChordMethod()
-{
-   auto step = std::pow(10, -3);
-   auto limit = std::pow(10, -14);
-   std::cout << "ChordMethod\n";
-   std::cout << "\tEps" << "\t\tRoot's value\t" << "\tEstimation accuracy" << std::endl;
-   for (auto eps = std::pow(10, -2); eps > limit; eps *= step)
-   {
-      std::cout << std::fixed << std::setprecision(15) << eps << '\t' << m_x << '\t' << std::abs(getByChordMethod(eps) - m_x) << std::endl;
-   }
-}
 
-void Task::createTableForTangentMethod()
-{
-   auto step = std::pow(10, -3);
-   auto limit = std::pow(10, -14);
-   std::cout << "TangentMethod\n";
-   std::cout << "\tEps" << "\t\tRoot's value\t" << "\tEstimation accuracy" << std::endl;
-   for (auto eps = std::pow(10, -2); eps > limit; eps *= step)
-   {
-      std::cout << std::fixed << std::setprecision(15) << eps << '\t' << m_x << '\t' << std::abs(getByTangentMethod(eps) - m_x) << std::endl;
-   }
-}
-
-void Task::createThirdTable()
+void Task::createThirdTable() const
 {
    std::cout << "TASK_3 - Speed tables" << std::endl << std::endl;
    std::cout << std::setw(17) << "eps" << std::setw(17) << "IterationMethod"
-                      << std::setw(17)  << "BisectionMethod" 
-                      << std::setw(17)  << "ChordMethod" 
-                      << std::setw(17)  << "TangentMethod" << std::endl << std::endl;
-   
-   for (double eps = pow(10, -2); eps > pow(10, -14); eps = eps * pow(10, -3))
+      << std::setw(17) << "BisectionMethod" << std::endl << std::endl;
+   const auto step = 1e-3;
+   const auto limit = 1e-14;
+   auto i = 0;
+   for (auto eps = 1e-2; eps > limit; eps *=step, ++i)
    {
-      getByIterationMethod(eps);
-      getByBisectionMethod(m_a, m_b , eps);
-      getByChordMethod(eps);
-      getByTangentMethod(eps);
-      std::cout << std::fixed << std::setprecision(14) << std::setw(16) << eps 
-                              << std::setprecision(0)  << std::setw(17)  << m_nForIterationMethod
-                                                       << std::setw(17)  << m_nForBisectionMethod 
-                                                       << std::setw(17)  << m_nForChordMethod 
-                                                       << std::setw(17)  << m_nForTangentMethod << std::endl;
+      std::cout << std::fixed << std::setprecision(14) << std::setw(16) << eps
+         << std::setprecision(0) << std::setw(17) << m_nForIterationMethod[i]
+         << std::setw(17) << m_nForBisectionMethod[i] << std::endl;
    }
 }
 
-double Task::getByIterationMethod(double eps)
+std::tuple<double, double> Task::getByIterationMethod(double a, double b, double eps) const
 {
-   m_nForIterationMethod = 0;
-   double lambda = 1 / m_M;
-   double q = 1 - m_m / m_M;
-   double x = m_a;
+   auto n = 0;
+   const auto lambda = 1 / m_M;
+   const auto q = 1 - fabs(m_m / m_M);
+   auto x = (b + a)/2;
+   auto x1 = x;
    auto getCorrectFunctionValue = [&](double x)
    {
-      return x - lambda*getFunctionValue(x);
+      return x - lambda * getFunctionValue(x);
    };
-   auto limit = ((1 - q) * eps / q);
-   while (std::abs(getCorrectFunctionValue(x) - x) > limit)
+   const auto limit = ((1 - q) * eps / q);
+   do
    {
-      x = getCorrectFunctionValue(x);
-      ++m_nForIterationMethod;
-   }
-   return x;
+      x = x1;
+      x1= getCorrectFunctionValue(x);
+      ++n;
+   } while (fabs(x1 - x) > limit);
+   m_nForIterationMethod.push_back(n);
+   return std::make_tuple(x1, fabs(fabs(x1) - fabs(x)) * limit);
 }
 
 double Task::getFunctionValue(double x) const
 {
    return log(2 + sin(x)) - (x / 10) - 0.5;
 }
-
-double Task::getByBisectionMethod(double a, double b, double eps)
+double Task::getFirstDerivativeValue(double x) const
 {
-   m_nForBisectionMethod = 0;
+   return   cos(x) / (2 + sin(x)) - 0.1;
+}
+std::tuple<double, double> Task::getByBisectionMethod(double a, double b, double eps) const
+{
+   int n = 0;
    double x = 0.0;
-   while (std::abs(getFunctionValue(x) - eps) > eps)
+   do
    {
       x = (a + b) / 2;
       if (getFunctionValue(x) * getFunctionValue(a) < 0)
       {
          b = x;
       }
-      else if (getFunctionValue(x) * getFunctionValue(b) < 0)
+      if(getFunctionValue(x) * getFunctionValue(b) < 0)
       {
          a = x;
       }
-      ++m_nForBisectionMethod;
-   }
-   return x;
+      ++n;
+   } while (fabs(b - a) >= 2 * eps);
+   x =  (a + b) / 2;
+   m_nForBisectionMethod.push_back(n);
+   return std::make_tuple(x, fabs(b - a)/2);
 }
 
-double Task::getByChordMethod(double eps)
-{
-   m_nForChordMethod = 0;
-   double x = std::abs(m_a - m_c) < std::abs(m_b - m_c) ? m_b : m_a;
-   double m = m_b;// for current function because min(f`(x)) in [a;b] is when x = b
-   while (std::abs(getFunctionValue(x) / m_b) > eps) 
-   {
-      x = x - (getFunctionValue(x)*(x - m_c)) / (getFunctionValue(x) - getFunctionValue(m_c));
-      ++m_nForChordMethod;
-   }
-   
-   return x;
-}
 
-double Task::getByTangentMethod(double eps)
-{
-   auto derivative = [](double x)
-   {
-      return (cos(x) / (2 + sin(x))) - 1 / 10;
-   };//derivative for the given function
 
-   m_nForTangentMethod = 0;
-
-   //x must be equal to tne of the line where f(x)f(x)'' > 0
-   //for current function it can be any of two values
-   double x = m_a;;
-   double m = m_b;// for current function because min(f`(x)) in [a;b] is when x = b
-   while (std::abs(getFunctionValue(x) / m_b) > eps)
-   {
-      x = x - (getFunctionValue(x) / derivative(x));
-      ++m_nForTangentMethod;
-   }
-
-   return x;
-}
 
 int main()
 {
-   double x1 = -2.3677;
-   double a1 = -2.4;
-   double b1 = -2.2;
-   double c = -2.375; // from f(x)*f(x)`` > 0
-   double m1 = -0.593915;
-   double M1 = -0.656718;
-   Task task(x1 , a1 , b1 , m1 , M1 , c);
+   double a1 = -10;
+   double b1 = 10;
+   Task task(a1, b1);
    task.createTableForIterationMethod();
    task.createTableForBisectionMethod();
-   task.createTableForChordMethod();
-   task.createTableForTangentMethod();
    task.createThirdTable();
    std::cin.get();
 }
