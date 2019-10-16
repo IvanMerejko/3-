ec652f582e1957108172c95dbdf27ed7ad46f498
IvanMerejko <32937297+IvanMerejko@users.noreply.github.com>
2019-10-16 15:12:46 +0300

    added Insert and Delete actions

--- БД/lab2/Task1.py
+++ БД/lab2/Task1.py
@@ -1,10 +1,187 @@
 from tkinter import *
 import constant as con
 
+label_height = 15
+
+
+def create_assortment_menu(assortment_id, commodity_id, shop_id):
+    con.create_label("assortment_id", label_height, 2, 1, 4, 1, 1, "white")
+    con.create_entry(assortment_id, 2, 4)
+    con.create_label("commodity_id", label_height, 2, 1, 5, 1, 1, "white")
+    con.create_entry(commodity_id, 2, 5)
+    con.create_label("shop_i", label_height, 2, 1, 6, 1, 1, "white")
+    con.create_entry(shop_id, 2, 6)
+
+
+def create_commodity_menu(commodity_id, name, price):
+    con.create_label("commodity_id", label_height, 2, 1, 4, 1, 1, "white")
+    con.create_entry(commodity_id, 2, 4)
+    con.create_label("name", label_height, 2, 1, 5, 1, 1, "white")
+    con.create_entry(name, 2, 5)
+    con.create_label("price", label_height, 2, 1, 6, 1, 1, "white")
+    con.create_entry(price, 2, 6)
+
+
+def create_customer_menu(customer_id, name, raiting, worker_id):
+    con.create_label("customer_id", label_height, 2, 1, 4, 1, 1, "white")
+    con.create_entry(customer_id, 2, 4)
+    con.create_label("name", label_height, 2, 1, 5, 1, 1, "white")
+    con.create_entry(name, 2, 5)
+    con.create_label("raiting", label_height, 2, 1, 6, 1, 1, "white")
+    con.create_entry(raiting, 2, 6)
+    con.create_label("worker_id", label_height, 2, 1, 7, 1, 1, "white")
+    con.create_entry(worker_id, 2, 7)
+
+
+def create_order_menu(customer_id, order_id, assortment_id):
+    con.create_label("customer_id", label_height, 2, 1, 4, 1, 1, "white")
+    con.create_entry(customer_id, 2, 4)
+    con.create_label("order_id", label_height, 2, 1, 5, 1, 1, "white")
+    con.create_entry(order_id, 2, 5)
+    con.create_label("assortment_id", label_height, 2, 1, 6, 1, 1, "white")
+    con.create_entry(assortment_id, 2, 6)
+
+
+def create_shop_menu(shop_id, monthly_profit, address):
+    con.create_label("shop_id", label_height, 2, 1, 4, 1, 1, "white")
+    con.create_entry(shop_id, 2, 4)
+    con.create_label("monthly_profit", label_height, 2, 1, 5, 1, 1, "white")
+    con.create_entry(monthly_profit, 2, 5)
+    con.create_label("address", label_height, 2, 1, 6, 1, 1, "white")
+    con.create_entry(address, 2, 6)
+
+
+def create_worker_menu(worker_id, shop_id, name, surname, position):
+    con.create_label("worker_id", label_height, 2, 1, 4, 1, 1, "white")
+    con.create_entry(worker_id, 2, 4)
+    con.create_label("shop_id", label_height, 2, 1, 5, 1, 1, "white")
+    con.create_entry(shop_id, 2, 5)
+    con.create_label("name", label_height, 2, 1, 6, 1, 1, "white")
+    con.create_entry(name, 2, 6)
+    con.create_label("surname", label_height, 2, 1, 7, 1, 1, "white")
+    con.create_entry(surname, 2, 7)
+    con.create_label("position", label_height, 2, 1, 8, 1, 1, "white")
+    con.create_entry(position, 2, 8)
+
+
+def create_delete_menu():
+    remove_unused_elements()
+    delete_part_fields = {con.Assortment_str: 'assortment_id',
+                          con.Commodity_str: 'commodity_id',
+                          con.Order_str: 'order_id',
+                          con.Worker_str: 'worker_id',
+                          con.Shop_str: 'shop_id',
+                          con.Customer_str: 'customer_id'}
+    current_field_name = delete_part_fields[con.current_table]
+    value = StringVar()
+    def delete_statement():
+        result = con.execute_query(con.delete_query.format(table=con.current_table,
+                                                           field=current_field_name,
+                                                            value=value.get()))
+        con.text_label['text'] = result
+    con.create_label(current_field_name, label_height, 2, 1, 4, 1, 1, "white")
+    con.create_entry(value, 2, 4)
+    con.create_button('delete', delete_statement, 1, 5)
+
+
+def remove_unused_elements():
+    positions = con.remove_dict_for_first_task[con.current_table]
+    positions += con.delete_position_for_remove
+    for row, column in positions:
+        con.remove_element(row, column)
+
+
+def create_assortment_insert():
+    assortment_id = StringVar()
+    commodity_id = StringVar()
+    shop_id = StringVar()
+    create_assortment_menu(assortment_id, commodity_id, shop_id)
+    def insert_callback():
+        result = con.execute_query(con.insert_queries[3].format(con.current_table, assortment_id.get(),
+                                                                commodity_id.get(), shop_id.get()))
+        con.text_label['text'] = result
+    con.create_button('Ok', insert_callback, 1, 7)
+
+
+def create_commodity_insert():
+    name = StringVar()
+    commodity_id = StringVar()
+    price = StringVar()
+    create_commodity_menu(commodity_id, name, price)
+    def insert_callback():
+        result = con.execute_query(con.insert_queries[3].format(con.current_table, commodity_id.get(),
+                                                                name.get(), price.get()))
+        con.text_label['text'] = result
+    con.create_button('Ok', insert_callback, 1, 7)
+
+
+def create_customer_insert():
+    customer_id = StringVar()
+    name = StringVar()
+    raiting = StringVar()
+    worker_id = StringVar()
+    create_customer_menu(customer_id, name, raiting, worker_id)
+    def insert_callback():
+        result = con.execute_query(con.insert_queries[4].format(con.current_table, customer_id.get(),
+                                                                raiting.get(), worker_id.get(), name.get()))
+        con.text_label['text'] = result
+    con.create_button('Ok', insert_callback, 1, 8)
+
+
+def create_order_insert():
+    customer_id = StringVar()
+    order_id = StringVar()
+    assortment_id = StringVar()
+    create_order_menu(customer_id, order_id, assortment_id)
+    def insert_callback():
+        result = con.execute_query(con.insert_queries[3].format(con.current_table, order_id.get(),
+                                                                customer_id.get(), assortment_id.get()))
+        con.text_label['text'] = result
+    con.create_button('Ok', insert_callback, 1, 7)
+
+
+def create_shop_insert():
+    shop_id = StringVar()
+    monthly_profit = StringVar()
+    address = StringVar()
+    create_shop_menu(shop_id, monthly_profit, address)
+    def insert_callback():
+        result = con.execute_query(con.insert_queries[3].format(con.current_table, shop_id.get(),
+                                                                monthly_profit.get(), address.get()))
+        con.text_label['text'] = result
+    con.create_button('Ok', insert_callback, 1, 7)
+
+
+def create_worker_insert():
+    worker_id = StringVar()
+    shop_id = StringVar()
+    name = StringVar()
+    surname = StringVar()
+    position = StringVar()
+    create_worker_menu(worker_id, shop_id, name, surname, position)
+    def insert_callback():
+        result = con.execute_query(con.insert_queries[3].format(con.current_table, worker_id.get(), shop_id.get(),
+                                                                name.get(), surname.get(), position.get()))
+        con.text_label['text'] = result
+    con.create_button('Ok', insert_callback, 1, 9)
+
+
+insert_creators = {con.Assortment_str: create_assortment_insert,
+                   con.Commodity_str: create_commodity_insert,
+                   con.Customer_str: create_customer_insert,
+                   con.Order_str: create_order_insert,
+                   con.Shop_str: create_shop_insert,
+                   con.Worker_str: create_worker_insert}
+
 
 def create_action_menu(table_name):
+    if len(con.current_table) != 0:
+        remove_unused_elements()
     con.current_table = table_name
-    con.create_button("Select", con.select, 1,3)
+    con.change_color(con.clicked_color_change[con.current_table])
+    con.create_button("Select", con.select, 1, 3)
+    con.create_button("Insert", insert_creators[con.current_table], 2, 3)
+    con.create_button("Delete", create_delete_menu, 3, 3)
 
 
 def assortment_table_use():
@@ -12,6 +189,7 @@ def assortment_table_use():
 
 
 def commodity_table_use():
+
     create_action_menu(con.Commodity_str)
 
 
--- БД/lab2/constant.py
+++ БД/lab2/constant.py
@@ -1,24 +1,62 @@
 from tkinter import *
 import psycopg2
-Assortment_str = "assortment"
-Commodity_str = "commodity"
-Customer_str = "customer"
-Order_str = "order"
-Shop_str = "shop"
-Worker_str = "worker"
 
-current_table = ""
+Assortment_str = 'assortment'
+Commodity_str = 'commodity'
+Customer_str = 'customer'
+Order_str = 'order'
+Shop_str = 'shop'
+Worker_str = 'worker'
+
+clicked_color_change = {Assortment_str: (2, 1),
+                        Commodity_str: (2, 2),
+                        Customer_str: (2, 3),
+                        Order_str: (2, 4),
+                        Shop_str: (2, 5),
+                        Worker_str: (2, 6)}
 
 
+remove_dict_for_first_task = {Assortment_str: [(4, 1), (5, 1), (6, 1), (4, 2), (5, 2), (6, 2), (7, 1)],
+                              Commodity_str: [(4, 1), (5, 1), (6, 1), (4, 2), (5, 2), (6, 2), (7, 1)],
+                              Customer_str: [(4, 1), (5, 1), (6, 1), (7, 1), (4, 2), (5, 2), (6, 2), (7, 2), (8, 1)],
+                              Order_str: [(4, 1), (5, 1), (6, 1), (4, 2), (5, 2), (6, 2), (7, 1)],
+                              Shop_str: [(4, 1), (5, 1), (6, 1), (4, 2), (5, 2), (6, 2), (7, 1)],
+                              Worker_str: [(4, 1), (5, 1), (6, 1), (7, 1), (8, 1), (4, 2), (5, 2), (6, 2), (7, 2), (8, 2), (9, 1)]}
+
+insert_queries = {3: 'INSERT INTO \"{0}\"VALUES ({1},{2},{3})',
+                  4: 'INSERT INTO \"{0}\"VALUES ({1},{2},{3},{4})',
+                  5: 'INSERT INTO \"{0}\"VALUES ({1},{2},{3},{4},{5})'}
+
+delete_query = 'Delete From \"{table}\" Where {field} = {value}'
+delete_position_for_remove = [(4, 1), (4, 2), (5, 1)]
+current_table = ''
+
+
+def create_label(name, width, height, column_number,row_number=1, columnspan=9, rowspan=5, color="light blue"):
+    label = Label(text=name, width=width, height=height, bg=color)
+    label.grid(row=row_number, column=column_number, columnspan=columnspan, rowspan=rowspan,pady=1)
+    return label
+
+
+def create_button(name, callback, column_number, row_number=1):
+    button = Button(text=name, command=callback)
+    button.grid(row=row_number, column=column_number, padx=5, pady=3)
+    return button
+
+
+def create_entry(var_text, column_number, row_number):
+    entry = Entry(textvariable=var_text)
+    entry.grid(row=row_number, column=column_number, padx=1, pady=1)
+    return entry
+
+HEIGHT = 800
 WIDTH = 600
-HEIGHT = 500
 window = Tk()
 window.minsize(width=100, height=100)
-window.geometry('600x500')
+window.geometry('{0}x{1}'.format(HEIGHT, WIDTH))
 window.title("lab2")
 window.grid_columnconfigure(4, minsize=100)
-text_label = Label(width=100, height=5, bg="light blue")
-text_label.grid(row=10, column=1, columnspan=9, rowspan=5)
+text_label = create_label('', 130, 20, 1, 10)
 connection = psycopg2.connect(user = "postgres",
                               password = "bdfy45231",
                               port = "1111",
@@ -29,14 +67,38 @@ cursor = connection.cursor()
 def select():
     print(current_table)
     cursor.execute("SELECT * FROM \"{0}\";".format(current_table))
-    records = cursor.fetchall()
-    text_label['text'] = records
-    # for record in records:
-    #     text_label['text'] += record
+    connection.commit()
+    result = None
+    text_label['text'] = ''
+    for record in cursor.fetchall():
+        text_label['text'] += str(record) + '\n'
 
 
 
-def create_button(name, callback, column_number, row_number=1):
-    print(name, row_number, column_number)
-    button = Button(text=name, command=callback)
-    button.grid(row=row_number, column=column_number, padx=5, pady=3)
+
+
+def remove_element(row, column):
+    for element in window.grid_slaves():
+        if element.grid_info()["row"] == row and element.grid_info()["column"] == column:
+            element.grid_forget()
+            return
+
+
+def change_color(pos):
+    row, column = pos[0], pos[1]
+    for element in window.grid_slaves():
+        if element.grid_info()["row"] == row and element.grid_info()["column"] == column:
+            element.config(bg="light blue")
+        else:
+            element.config(bg="white")
+
+
+def execute_query(query):
+    try:
+        cursor.execute(query)
+        connection.commit()
+        return 'Ok'
+    except Exception as err:
+        connection.commit()
+        return err
+
